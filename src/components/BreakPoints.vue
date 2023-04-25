4a13e5a5ca6ca98a0466d5975208149429f98615
Robert Baumgartner <robert.baumgartner@htlwienwest.at>
2023-04-25 16:30:02 +0200

    changed visual appearance

--- src/components/BreakPoints.vue
+++ src/components/BreakPoints.vue
@@ -1,13 +1,10 @@
 <template>
-  <div
-    class="bg-danger text-center text-light p-2 mx-auto mt-5 rounded rounded-3 bg-opacity-75"
-    style="width: 150px"
-  >
+  <small class="mt-5 text-muted d-flex justify-content-center"> --
     <span class="d-block d-sm-none">Extra Small (xs)</span>
     <span class="d-none d-sm-block d-md-none">Small (sm)</span>
     <span class="d-none d-md-block d-lg-none">Medium (md)</span>
     <span class="d-none d-lg-block d-xl-none">Large (lg)</span>
     <span class="d-none d-xl-block d-xxl-none">X-Large (xl)</span>
-    <span class="d-none d-xxl-block">XX-Large (xxl)</span>
-  </div>
+    <span class="d-none d-xxl-block">XX-Large (xxl)</span> --
+  </small>
 </template>
